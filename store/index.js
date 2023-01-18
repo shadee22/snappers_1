21bc6ba51e6c77f2720295af88ff1d9597f08f1c
Shadeer Sadikeen <shadeersadikeen@gmail.com>
2023-01-18 18:15:39 +0530

    removed firebase plugin

---
+++ store/index.js
@@ -0,0 +1,103 @@
+
+
+export const state = () => ({
+  list: [],
+  all_products: [],
+  discount_products:[{"orderId":0,"itemID":40306,"itemCode":"91287","name":"Munchee Super Cream Cracker 490g","longDescription":"MUNCHEE SUPER CREAM CRACKER 490G","amount":520,"imageUrl":"https://essstr.blob.core.windows.net/essimg/350x/Small/Pic91287.jpg","enlargeImageUrl":"","isFeatured":true,"isSponsored":false,"minQty":1,"maxQty":99,"slabQty":1,"stockInHand":70,"averageSale":0.2,"uom":"NO","isPromotionApplied":false,"promotionDiscountValue":0,"discountValue":20,"promoDetailIDs":null,"discountedTotal":0,"isAvailable":true,"quantity":0,"checkAge":false,"departmentCode":"G","subDepartmentCode":"GBS","categoryCode":"GBSF4","styleBorderColor":"","styleImageURL":"","isSellingToday":true,"isUpdateQuantity":false,"merchantID":1,"hasMoreDeals":0,"detailDescription":""},{"orderId":592,"itemID":45467,"itemCode":"15933","name":"Md Jam Wood Apple 500g","longDescription":"MD JAM WOOD APPLE 500G","amount":530,"imageUrl":"https://essstr.blob.core.windows.net/essimg/350x/Small/Pic15933.jpg","enlargeImageUrl":"","isFeatured":false,"isSponsored":false,"minQty":1,"maxQty":99,"slabQty":1,"stockInHand":4,"averageSale":0.07,"uom":"NO","isPromotionApplied":false,"promotionDiscountValue":0,"discountValue":30,"promoDetailIDs":null,"discountedTotal":0,"isAvailable":true,"quantity":0,"checkAge":false,"departmentCode":"G","subDepartmentCode":"GJA","categoryCode":"GJA0G","styleBorderColor":"","styleImageURL":"","isSellingToday":true,"isUpdateQuantity":false,"merchantID":1,"hasMoreDeals":0,"detailDescription":""},{"orderId":1,"itemID":76755,"itemCode":"123804","name":"Ratthi 3 In 1 Kiri The 200g","longDescription":"RATTHI 3 IN 1 KIRI THE 200G","amount":440,"imageUrl":"https://essstr.blob.core.windows.net/essimg/350x/Small/Pic123804.jpg","enlargeImageUrl":"","isFeatured":true,"isSponsored":false,"minQty":1,"maxQty":999,"slabQty":1,"stockInHand":28,"averageSale":0.2,"uom":"NO","isPromotionApplied":false,"promotionDiscountValue":0,"discountValue":5,"promoDetailIDs":null,"discountedTotal":0,"isAvailable":true,"quantity":0,"checkAge":false,"departmentCode":"B","subDepartmentCode":"BZH","categoryCode":"BZH15","styleBorderColor":"","styleImageURL":"","isSellingToday":true,"isUpdateQuantity":false,"merchantID":1,"hasMoreDeals":0,"detailDescription":""},{"orderId":616,"itemID":45912,"itemCode":"3429","name":"Md Jam Low Sugar Mixed Fruit 330g","longDescription":"MD JAM LOW SUGAR MIXED FRUIT 330G","amount":550,"imageUrl":"https://essstr.blob.core.windows.net/essimg/350x/Small/Pic3429.jpg","enlargeImageUrl":"","isFeatured":false,"isSponsored":false,"minQty":1,"maxQty":99,"slabQty":1,"stockInHand":6,"averageSale":0.07,"uom":"NO","isPromotionApplied":false,"promotionDiscountValue":0,"discountValue":5,"promoDetailIDs":null,"discountedTotal":0,"isAvailable":true,"quantity":0,"checkAge":false,"departmentCode":"G","subDepartmentCode":"GJA","categoryCode":"GJA0G","styleBorderColor":"","styleImageURL":"","isSellingToday":true,"isUpdateQuantity":false,"merchantID":1,"hasMoreDeals":0,"detailDescription":""},{"orderId":477,"itemID":42994,"itemCode":"10455","name":"Sunquick Orange Jumbo 700ml","longDescription":"SUNQUICK ORANGE JUMBO 700ML","amount":1130,"imageUrl":"https://essstr.blob.core.windows.net/essimg/350x/Small/Pic10455.jpg","enlargeImageUrl":"","isFeatured":false,"isSponsored":false,"minQty":1,"maxQty":99,"slabQty":1,"stockInHand":19,"averageSale":0.07,"uom":"NO","isPromotionApplied":false,"promotionDiscountValue":0,"discountValue":5,"promoDetailIDs":null,"discountedTotal":0,"isAvailable":true,"quantity":0,"checkAge":false,"departmentCode":"B","subDepartmentCode":"BZG","categoryCode":"BZG0K","styleBorderColor":"","styleImageURL":"","isSellingToday":true,"isUpdateQuantity":false,"merchantID":1,"hasMoreDeals":0,"detailDescription":""},{"orderId":1556,"itemID":62122,"itemCode":"110799","name":"Keells Wheat Flour 1kg","longDescription":"KEELLS WHEAT FLOUR 1KG","amount":420,"imageUrl":"https://essstr.blob.core.windows.net/essimg/350x/Small/Pic110799.jpg","enlargeImageUrl":"","isFeatured":false,"isSponsored":false,"minQty":1,"maxQty":2,"slabQty":1,"stockInHand":39,"averageSale":0.6,"uom":"NO","isPromotionApplied":false,"promotionDiscountValue":0,"discountValue":20,"promoDetailIDs":null,"discountedTotal":0,"isAvailable":true,"quantity":0,"checkAge":false,"departmentCode":"G","subDepartmentCode":"GFU","categoryCode":"GFU04","styleBorderColor":"","styleImageURL":"","isSellingToday":true,"isUpdateQuantity":false,"merchantID":1,"hasMoreDeals":0,"detailDescription":""},{"orderId":490,"itemID":43390,"itemCode":"109280","name":"Kotmale Feta Cheese Plain 100g","longDescription":"KOTMALE FETA CHEESE PLAIN 100G","amount":480,"imageUrl":"https://essstr.blob.core.windows.net/essimg/350x/Small/Pic109280.jpg","enlargeImageUrl":"","isFeatured":false,"isSponsored":false,"minQty":1,"maxQty":99,"slabQty":1,"stockInHand":0,"averageSale":0.07,"uom":"NO","isPromotionApplied":false,"promotionDiscountValue":0,"discountValue":10,"promoDetailIDs":null,"discountedTotal":0,"isAvailable":true,"quantity":0,"checkAge":false,"departmentCode":"C","subDepartmentCode":"CG0","categoryCode":"CG0FC","styleBorderColor":"","styleImageURL":"","isSellingToday":true,"isUpdateQuantity":false,"merchantID":1,"hasMoreDeals":0,"detailDescription":""},{"orderId":92,"itemID":77039,"itemCode":"123427","name":"Maliban Yahaposha Nutri Fix 200g","longDescription":"MALIBAN YAHAPOSHA NUTRI FIX 200G","amount":160,"imageUrl":"https://essstr.blob.core.windows.net/essimg/350x/Small/Pic123427.jpg","enlargeImageUrl":"","isFeatured":false,"isSponsored":false,"minQty":1,"maxQty":999,"slabQty":1,"stockInHand":58,"averageSale":0.07,"uom":"NO","isPromotionApplied":true,"promotionDiscountValue":16,"discountValue":25,"promoDetailIDs":null,"discountedTotal":0,"isAvailable":true,"quantity":0,"checkAge":false,"departmentCode":"G","subDepartmentCode":"GCE","categoryCode":"GCE5X","styleBorderColor":"","styleImageURL":"","isSellingToday":true,"isUpdateQuantity":false,"merchantID":1,"hasMoreDeals":0,"detailDescription":""},{"orderId":839,"itemID":49809,"itemCode":"113096","name":"Anchor Plus For 1-5 Year Child. 350g","longDescription":"ANCHOR PLUS FOR 1-5 YEAR CHILD. 350G","amount":1190,"imageUrl":"https://essstr.blob.core.windows.net/essimg/350x/Small/Pic113096.jpg","enlargeImageUrl":"","isFeatured":false,"isSponsored":false,"minQty":1,"maxQty":99,"slabQty":1,"stockInHand":18,"averageSale":0.07,"uom":"NO","isPromotionApplied":false,"promotionDiscountValue":0,"discountValue":30,"promoDetailIDs":null,"discountedTotal":0,"isAvailable":true,"quantity":0,"checkAge":false,"departmentCode":"H","subDepartmentCode":"HB3","categoryCode":"HB3BM","styleBorderColor":"","styleImageURL":"","isSellingToday":true,"isUpdateQuantity":false,"merchantID":1,"hasMoreDeals":0,"detailDescription":""},{"orderId":1044,"itemID":55133,"itemCode":"10602","name":"Imorich Cookie Cream 1L","longDescription":"IMORICH COOKIE CREAM 1L","amount":1100,"imageUrl":"https://essstr.blob.core.windows.net/essimg/350x/Small/Pic10602.jpg","enlargeImageUrl":"","isFeatured":false,"isSponsored":false,"minQty":1,"maxQty":99,"slabQty":1,"stockInHand":2,"averageSale":0.07,"uom":"NO","isPromotionApplied":false,"promotionDiscountValue":0,"discountValue":10,"promoDetailIDs":null,"discountedTotal":0,"isAvailable":true,"quantity":0,"checkAge":false,"departmentCode":"D","subDepartmentCode":"DDE","categoryCode":"DDEDP","styleBorderColor":"","styleImageURL":"","isSellingToday":true,"isUpdateQuantity":false,"merchantID":1,"hasMoreDeals":0,"detailDescription":""},{"orderId":126,"itemID":77152,"itemCode":"124093","name":"Good Look Hair Gel Aloe Vera Green 30ml","longDescription":"GOOD LOOK HAIR GEL ALOE VERA GREEN 30ML","amount":275,"imageUrl":"https://essstr.blob.core.windows.net/essimg/350x/Small/Pic124093.jpg","enlargeImageUrl":"","isFeatured":false,"isSponsored":false,"minQty":1,"maxQty":999,"slabQty":1,"stockInHand":0,"averageSale":0,"uom":"NO","isPromotionApplied":false,"promotionDiscountValue":0,"discountValue":30,"promoDetailIDs":null,"discountedTotal":0,"isAvailable":true,"quantity":0,"checkAge":false,"departmentCode":"H","subDepartmentCode":"H7I","categoryCode":"H7IIX","styleBorderColor":"","styleImageURL":"","isSellingToday":true,"isUpdateQuantity":false,"merchantID":1,"hasMoreDeals":0,"detailDescription":""},{"orderId":1206,"itemID":56442,"itemCode":"118026","name":"Bellybees Yellow Juice 200ml","longDescription":"BELLYBEES YELLOW JUICE 200ML","amount":270,"imageUrl":"https://essstr.blob.core.windows.net/essimg/350x/Small/Pic118026.jpg","enlargeImageUrl":"","isFeatured":false,"isSponsored":false,"minQty":1,"maxQty":99,"slabQty":1,"stockInHand":0,"averageSale":0,"uom":"NO","isPromotionApplied":false,"promotionDiscountValue":0,"discountValue":25,"promoDetailIDs":null,"discountedTotal":0,"isAvailable":true,"quantity":0,"checkAge":false,"departmentCode":"B","subDepartmentCode":"BRE","categoryCode":"BREZN","styleBorderColor":"","styleImageURL":"","isSellingToday":true,"isUpdateQuantity":false,"merchantID":1,"hasMoreDeals":0,"detailDescription":""}],
+  catos: [],
+no_image_url: 'https://t4.ftcdn.net/jpg/04/70/29/97/360_F_470299797_UD0eoVMMSUbHCcNJCdv2t8B2g1GVqYgs.jpg',
+  tab: 0,
+  color: 0,
+  initial_banner: {
+    status: true,
+  },
+})
+
+export const mutations = {
+  close_initial_banner(state){
+    state.initial_banner.status = false
+
+  },
+  add_to_discount(state, data) {
+    let selected = state.all_products.find(el => el.name == data.item)
+    selected.discountValue = data.discount;
+    return state.discount_products.push(selected);
+  },
+  increment(state) {
+    // mutate state
+    state.count++
+  },
+  change_color(state) {
+    if (state.color < 4) {
+      state.color += 1;
+    } else {
+      state.color = 0;
+    }
+  },
+  load_all_keels_products(state, data) {
+    // console.log(data.products);
+    state.catos = data.catos;
+    state.all_products = data.products;
+  },
+
+  switch_tab(state, tab) {
+    return state.tab = tab;
+  },
+
+  add_item(state, data) {
+    let check = state.list.find(e => {
+      return e.name == data.name;
+    });
+    if (check != undefined) {
+      check.quantity += 1;
+    } else {
+      state.list.push(data);
+    }
+  },
+  add_quantity(state, name) {
+    let current_item = state.list.find(el => {
+      return el.name == name
+    })
+    current_item.quantity += 1;
+  },
+  remove_item(state, item) {
+    // console.log("list" , state.list);
+    // console.log("item" , item);
+    let find = state.list.find((el) => el.name === item.name);
+    state.list.splice(state.list.indexOf(find), 1);
+
+  },
+  remove_quantity(state, name) {
+    let current_item = state.list.find(el => {
+      return el.name == name
+    })
+    if (current_item.quantity > 1) {
+      current_item.quantity -= 1;
+    }
+
+  },
+
+
+
+
+}
+export const getters = {
+  get_total: (state) => {
+    let total = 0;
+    state.list.forEach(el => {
+      total += el.price * el.quantity;
+    });
+
+    let obj = {}
+    obj['total'] = total.toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    obj['total_with_discount'] = (total - total * 0.17).toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    obj['discount'] = (total * 0.17).toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    obj['discount_without_comma'] = (total * 0.17).toFixed(2)
+
+
+    return obj;
+  },
+  get_current_quantity: (state, name) => {
+    return state.list;
+  }
+}
