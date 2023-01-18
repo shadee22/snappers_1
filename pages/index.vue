73c4e1cb5e6ed7cee90070984c0cc1aaa43e81d0
Shadeer Sadikeen <shadeersadikeen@gmail.com>
2023-01-18 15:44:12 +0530

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <nav class="navbar">
+      <h1 class="navbar-brand">Checking Snappers</h1>
+      <button class="">CHekcing </button>
+    </nav>
+  </div>
+</template>
+
+<style>
+/* Basic Twitter-like styling */
+.navbar {
+  background-color: #1da1f2;
+  color: #fff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 50px;
+}
+
+.navbar-brand {
+  font-size: 24px;
+  font-weight: bold;
+}
+</style>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
